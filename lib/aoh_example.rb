d89ecc520121fea2fb608fee64eee68f443ebd4b
Ian McKenzie <imckenzie777@gmail.com>
2020-05-03 19:02:21 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -14,8 +14,8 @@ def assembled_aoh
     JOELLE_VD,# = { name:  "Joelle van Dyne", occupation: "Radio Personality" },
     PAT_M,# =  { name:  "Pat Monteseian", occupation: "Staff" },
     KATE_G,# = { name:  "Kate Gompert", occupation: "None" },
-    BRUCE_G# = { name:  "Bruce Green", occupation: "Fan of Mildred" }
-  ].sort
+    BRUCE_G.sort# = { name:  "Bruce Green", occupation: "Fan of Mildred" }
+  ]
 end
 
 def literal_aoh
